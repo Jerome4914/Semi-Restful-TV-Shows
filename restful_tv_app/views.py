7d23d391ff02dc285b6be915327ee25e9dc44b76
jerome4914 <jeremyballew@hotmail.com>
2021-07-10 17:09:49 -0600

    added validations

--- restful_tv_app/views.py
+++ restful_tv_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
 
 # Create your views here.
@@ -16,27 +17,41 @@ def new(request):
     return render(request, "new_show.html")
 
 def shows_create(request):
-    Show.objects.create(
-        title=request.POST['title'],
-        network=request.POST['network'],
-        release_date=request.POST['release_date'],
-        description=request.POST['description'],
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        Show.objects.create(
+            title=request.POST['title'],
+            network=request.POST['network'],
+            release_date=request.POST['release_date'],
+            description=request.POST['description'],                    
+        # show.save(),
+        # messages.success(request, "Show successfully created")
     )
     return redirect('/shows')
 
 def edit(request, id):
     one_show = Show.objects.get(id=id)
     context = {
-        'show': one_show
-    }
+        'show': one_show        
+    }    
     return render(request, 'edit.html', context)
 
 def update(request, id):
-    update = Show.objects.get(id=id)
-    update.title = request.POST['title']
-    update.release_date = request.POST['release_date']
-    update.network = request.POST['network']
-    update.description = request.POST['description']
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        update = Show.objects.get(id=id)
+        update.title = request.POST['title']
+        update.release_date = request.POST['release_date']
+        update.network = request.POST['network']
+        update.description = request.POST['description']
 
     return redirect('/shows')
 
